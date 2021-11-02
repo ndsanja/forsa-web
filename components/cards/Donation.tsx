37dfbfce31772a8e91c275893082042f0120a33d
ndsanja <ndsanja@gmail.com>
2021-11-02 19:10:20 +0700

    add donation card

---
+++ components/cards/Donation.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+const Donation = () => {
+  return (
+    <div className="flex flex-col space-y-4 border">
+      <div className="h-full w-full aspect-w-16 aspect-h-9">
+        <img
+          src="https://media.istockphoto.com/photos/diverse-group-of-elementary-school-kids-go-back-to-school-wearing-picture-id1270794789?b=1&k=20&m=1270794789&s=170667a&w=0&h=bWb7fDf8b3OuTBEctKb4mHqI3oVuWfbAFY4By_3GLvc="
+          alt="gambar"
+        />
+      </div>
+      <div className="flex flex-col space-y-2 px-4">
+        <div>
+          <h6 className="text-label-small uppercase tracking-widest">
+            Donasi Umum
+          </h6>
+          <div className="flex space-x-2 text-body-small">
+            <h6>Oleh :</h6>
+            <p className="font-semibold">FORSA SMANGAT</p>
+          </div>
+        </div>
+        <div className="pb-2 space-y-3">
+          <h1 className="text-title-large">
+            Donasi Untuk Almamater: Bantu FORSA untuk selalu menebar manfaat
+            untuk almamater
+          </h1>
+          <p className="font-dinamit-light text-body-medium">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi minima
+            adipisci, recusandae animi aut magni earum incidunt praesentium
+            veniam neque deleniti dignissimos ullam cum labore repellat harum
+            dolores pariatur repudiandae.
+          </p>
+        </div>
+        <div className="space-y-2 font-dinamit-light text-body-medium border-t border-b py-4">
+          <div className="flex justify-between">
+            <p>Rp 24,700,111.00</p>
+            <p>18.30%</p>
+          </div>
+          <div className="h-2 w-full bg-gray-300">
+            <div className="h-full w-[18.30%] bg-green-500" />
+          </div>
+          <div className="flex justify-between">
+            <div>
+              <p>Rp 135,000,000.00</p>
+              <p>Goal</p>
+            </div>
+            <p>58 Days to Go</p>
+          </div>
+        </div>
+        <div className="py-4 border-b w-full flex justify-center">
+          <button className="px-4 py-2 bg-green-500 text-label-small text-white font-dinamit-light">
+            Donasi Sekarang
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Donation;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,11 @@ import RegularCard from "../components/cards/RegularCard";
 import Events from "../components/cards/Events";
 import BigCard from "../components/cards/BigCard";
 import SectionTitle from "../components/SectionTitle";
+import Donation from "../components/cards/Donation";
 
 const Home: NextPage = () => {
   return (
-    <div className="container max-w-screen-3xl border border-red-500 w-screen bg-white pb-40 space-y-20">
+    <div className="container max-w-screen-3xl border border-red-500 w-screen bg-white pb-40 space-y-12">
       <div className="mx-5 md:mx-12 lg:mx-20 ">
         <div className="flex flex-col md:flex-row-reverse space-y-16 md:space-y-0">
           <div className="md:w-[70%] grid md:grid-cols-2 gap-6 md:gap-8 lg:gap-14 ">
@@ -22,7 +23,7 @@ const Home: NextPage = () => {
           </div>
           <div className=" md:w-[30%] md:pr-[10%]  md:pt-[30%] md:space-y-4 -mr-5 md:mr-0">
             <h3 className="font-dinamit-medium uppercase tracking-widest text-label-medium">
-              today Events
+              forsa Events
             </h3>
             <div className="flex md:flex-col space-x-10 md:space-x-0  md:space-y-10 overflow-x-scroll md:overflow-x-hidden w-full">
               <Events />
@@ -33,6 +34,11 @@ const Home: NextPage = () => {
         </div>
       </div>
       <SectionTitle />
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 mx-5 md:mx-12 lg:mx-20 gap-8 lg:gap-14">
+        <Donation />
+        <Donation />
+        <Donation />
+      </div>
     </div>
   );
 };
